8c1903d48c8642737a65ffc5f3aec17524f1d910
Akim <akimkom@protonmail.com>
2022-08-02 16:04:43 +0300

    Made different signals for volume and layout

--- config.h
+++ config.h
@@ -2,14 +2,14 @@
 static const Block blocks[] = {
     /*Icon*/    /*Command*/     /*Update Interval*/ /*Update Signal*/
     {"",        "average_temp",         1,                 0},
-    {"",        "volume",               0,                 10}, // The command that sends this signal is "kill -44 $(pidof dwmblocks)". No idea why 44.
+    {"",        "volume",               0,                 10}, // The command that sends this signal is "kill -44 $(pidof dwmblocks)". Signal=kill_arg-34.
     {"",        "wifi",                 3,                 0},
     {"",        "speed",                1,                 0},
     // {"",        "uptime_",              1,                 0},
     {"",        "battery",              2,                 0},
     {"",        "date_",               60,                 0},
     {"",        "time_",                1,                 0},
-    {"",        "layout",               0,                 10},
+    {"",        "layout",               0,                 11},
 };
 
 static char *delim = "   ";
