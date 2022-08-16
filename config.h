cc8aeee6f3433dc5d31c1e273a51fb8d9ecd63b2
Akim <akimkom@protonmail.com>
2022-08-16 19:10:15 +0300

    Made brightness icon dynamic + minor changes

--- config.h
+++ config.h
@@ -9,15 +9,15 @@
 static const Block blocks[] = {
     /*Icon*/    /*Command*/     /*Update Interval*/ /*Update Signal*/
     {"",        "brightness",           0,                 12},
-    {"",        "uptime_",              1,                 0},
-    {"",        "average_temp",         1,                 0},
+    {"",        "volume",               0,                 10}, // The command that sends this signal is "kill -44 $(pidof dwmblocks)". Signal=kill_arg-34.
     {"",        "wifi",                 3,                 0},
     {"",        "speed",                1,                 0},
-    {"",        "volume",               0,                 10}, // The command that sends this signal is "kill -44 $(pidof dwmblocks)". Signal=kill_arg-34.
-    {"",        "date_",               60,                 0},
-    {"",        "layout",               0,                 11},
-    {"",        "time_",                1,                 0},
+    {"",        "uptime_",              1,                 0},
+    {"",        "average_temp",         1,                 0},
     {"",        "battery",              2,                 0},
+    {"",        "layout",               0,                 11},
+    {"",        "date_",               60,                 0},
+    {"",        "time_",                1,                 0},
 };
 
 static char *delim = "   ";
