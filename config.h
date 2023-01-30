9c54d8942e5f0e1a7e71d17d962968d575a743ba
Akim <akimkom@protonmail.com>
2023-01-30 16:21:47 +0000

    Added bluetooth script

--- config.h
+++ config.h
@@ -4,6 +4,7 @@ static const Block blocks[] = {
     {"",        "speed",                1,                 0},
     {"",        "uptime_",              1,                 0},
     {"",        "average_temp",         1,                 0},
+    {"",        "bluetooth",            3,                 0},
     {"",        "brightness",           0,                 12},
     {"",        "volume",               0,                 10}, // The command that sends this signal is "kill -44 $(pidof dwmblocks)". Signal=kill_arg-34.
     {"",        "battery",              2,                 0},
